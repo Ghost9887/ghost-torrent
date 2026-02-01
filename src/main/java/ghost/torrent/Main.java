a1965f7c5d75b32f24e8dd3fdc673bd7c08b6cbc
Ghost <duchojakub987@gmail.com>
2026-02-01 08:13:12 +0100

    args

--- src/main/java/ghost/torrent/Main.java
+++ src/main/java/ghost/torrent/Main.java
@@ -2,6 +2,22 @@ package ghost.torrent;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length < 1) {
+            System.out.println("No arguments passed");
+            System.exit(-1);
+        }
+        else if (args.length > 1) {
+            System.out.println("Too many arguments passed");
+            System.exit(-1);
+        }
+        else {
+            printArgs(args);
+        }
+    }
+
+    private static void printArgs(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
