ad2e9631ae756598b7b315e287654e987add1bb0
Ghost <duchojakub987@gmail.com>
2026-02-04 15:26:48 +0100

    encode

--- src/main/java/ghost/bencode/Main.java
+++ src/main/java/ghost/bencode/Main.java
@@ -7,18 +7,34 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 1) {
+        if (args.length == 0) {
             System.out.println("No file given");
+            System.out.println("usage: <filename> <action>");
             System.exit(1);
-        }else if(args.length > 1) {
+        }else if(args.length == 1) {
+            System.out.println("No action given");
+            System.out.println("usage: <filename> <action>");
+            System.exit(1);
+        }
+        else if(args.length > 2) {
             System.out.println("Too many args given");
+            System.out.println("usage: <filename> <action>");
             System.exit(1);
         }else {
             try {
-                byte[] torrentBytes = Files.readAllBytes(Path.of(args[0]));
-                String decoded = new Bencode(torrentBytes).decode();
-                System.out.println("Decoded: " + decoded);
-            }catch (IOException e){
+                if (args[1].equals("decode")) {
+                    byte[] content = Files.readAllBytes(Path.of(args[0]));
+                    System.out.println("Decoded: " + new Bencode(content).decode());
+                }
+                else if (args[1].equals("encode")) {
+                    byte[] content = Files.readAllBytes(Path.of(args[0]));
+                    System.out.println("Encoded: " + new Bencode(content).encode());
+                }
+                else {
+                    System.out.println("Invalid action try 'decode' or 'encode'");
+                    System.exit(1);
+                }
+            }catch (IOException e) {
                 System.out.println(e);
             }
         }
